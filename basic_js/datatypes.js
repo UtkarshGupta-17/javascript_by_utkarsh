9b3b640dbeee726fe6c88d61d824297dc2096482
UtkarshGupta-17 <utkarshgupta1735@gmail.com>
2026-03-14 08:53:08 +0000

    datatyprs and variables

---
+++ basic_js/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all JS code a newer version 
+//alert("hello") we are using node JS not brower 
+console.log(3+3)
+
+ console.log("hitesh")
+ //number=> rages to 2 to power 53
+ //bigint
+ //string =""
+ //boolean=true/false
+ //null=>standalone value 
+ //undefined
+ //symbol
+ //object
+ console.log(typeof null)// output is object (can be a interview question )
+ console.log(typeof undefined)// output is undefined
---
+++ basic_js/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553 
+let accountEmail="utkarsh@gmail.com"
+var accountPassword="124"
+accountCity="jaipur"
+ let accountState ;
+//not allowed as it is constant accountId =2//not allowed
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountState]);
+/*
+never store using var because it has issue of block scope and functional scope 
+*/
