693c062b155d890e08a57efa3cd24794792b2c7b
UtkarshGupta-17 <utkarshgupta1735@gmail.com>
2026-03-20 17:30:46 +0000

    strings

---
+++ basic_js/strings.js
@@ -0,0 +1,13 @@
+const newstring="utkarsh"
+const repocount=50
+//console.log(newstring+repocount); 
+console.log(`hello my name is ${newstring} and my repocount  is ${repocount}`);// this is called string itnerpolation
+const gamename=new String("hello")//console mai object hai string   
+console.log(gamename.length)
+const newstringg=gamename.substring(0,4)
+const anotherstring =gamename.slice(-2,4)//can be used using the negative avlues and it reads form the last then
+console.log(anotherstring)
+const trimex="  helo   ";
+console.log(trimex.trim())// trim removes the blank spaces from start and end of the string 
+
+
