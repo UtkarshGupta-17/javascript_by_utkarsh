c8b9fc64fd0325339d08b941353e72f316abfe05
UtkarshGupta-17 <utkarshgupta1735@gmail.com>
2026-03-28 08:04:26 +0000

    functions and scpoe

---
+++ basic2_js/scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // like this and no error comes 
+
+function addone(num){ //using this syntax we can call the function before its definition
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){ // but here error comes these all are cincept of funtion hoisting 
+    return num + 2
+}
